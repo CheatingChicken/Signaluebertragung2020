ae69b0359c08d3c63650e97975c84ea9ff461736
CheatingChicken <cheatingchicken@gmail.com>
2020-05-04 08:34:14 +0200

    changed descriptions

--- sine.m
+++ sine.m
@@ -1,4 +1,4 @@
 clear
-t = createTimeAxis(1e4,0.1);
+t = createTimeAxis(10,5);
 sin = sineWave(100,0,t);
 plot(t,sin)
--- sineWave.m
+++ sineWave.m
@@ -1,5 +1,5 @@
 function y = sineWave(freq, phase, time)
-%create sine wave vector
+%create sine wave vector with frequency,phase,timevector
 y = sin(2 * pi * freq * time + phase);
 end
 
