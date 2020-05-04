8dde970e62644e31cd13c522b47962b4f2f89b6b
CheatingChicken <cheatingchicken@gmail.com>
2020-05-04 09:36:27 +0200

    changes Signed-off-by: CheatingChicken <cheatingchicken@gmail.com>

--- Sprachsignal.m
+++ Sprachsignal.m
@@ -2,6 +2,5 @@ clear
 t=createTimeAxis(16e3,16e3*5);
 load('sprachsignal_16kSps.mat');
 plot(t,sprachsignal)
-mod = 1e9*sprachsignal;
-a = audioplayer(mod,16e3);
+a = audioplayer(10*sprachsignal,16e3);
 a.play
